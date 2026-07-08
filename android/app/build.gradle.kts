f96cb61e28120f9927e9a0d2cb7c7d5cb72b17a6
BenjaminDarko <benjamindarko775@gmail.com>
2026-07-08 18:48:05 -0400

    fix: upgrade desugar_jdk_libs to 2.1.4 as required by ota_update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,6 +47,6 @@ flutter {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation("androidx.media3:media3-common:1.4.1")
 }
